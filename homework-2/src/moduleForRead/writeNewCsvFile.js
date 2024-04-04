fb6519846c417bdcf97da3927a84ee9b85ea3cb0
Docenko Pasha <pashadocenko@gmail.com>
2024-04-04 17:28:36 +0300

    writeFileSync was replaced  to createWriteStream

--- homework-2/src/moduleForRead/writeNewCsvFile.js
+++ homework-2/src/moduleForRead/writeNewCsvFile.js
@@ -1,12 +1,29 @@
 const fs = require("fs");
 
 const writeNewCsvFile = (name, data) => {
-    try {
-        fs.writeFileSync(name, data);
-        console.log("Файл успешно обновлен.");
-    } catch (error) {
-        console.log("Ошибка при создании : ", error);
-    }
+    // old code
+    // try {
+    //     fs.writeFileSync(name, data);
+    //     console.log("Файл успешно обновлен.");
+    // } catch (error) {
+    //     console.log("Ошибка при создании : ", error);
+    // }
+
+    // new code
+
+    const writeStream = fs.createWriteStream(name);
+
+    writeStream.on('error', (error) => {
+        console.log("Ошибка при создании потока записи : ", error);
+    });
+    writeStream.write(data, 'utf8', (error) => {
+        if (error) {
+            console.log("Ошибка при записи : ", error);
+        } else {
+            console.log("Файл успешно обновлен.");
+        }
+        writeStream.end();
+    });
 };
 
 module.exports = writeNewCsvFile;
--- homework-2/src/writeFile.js
+++ homework-2/src/writeFile.js
@@ -7,12 +7,29 @@ const NAME_FILE = path.resolve(__dirname, "data.csv");
 const COUNT_OF_ROWS = 3000;
 
 const createCvfFile = (name, data) => {
-    try {
-        fs.writeFileSync(name, data);
-        console.log("Файл успешно создан.");
-    } catch (error) {
-        console.log("Ошибка при создании : ", error);
-    }
+    //Old code
+    // try {
+    //     fs.writeFileSync(name, data);
+    //     console.log("Файл успешно создан.");
+    // } catch (error) {
+    //     console.log("Ошибка при создании : ", error);
+    // }
+
+    //New code
+    const writeStream = fs.createWriteStream(name);
+
+    writeStream.on('error', (error) => {
+        console.log("Ошибка при создании потока записи : ", error);
+    });
+
+    writeStream.write(data, 'utf8', (error) => {
+        if (error) {
+            console.log("Ошибка при создании : ", error);
+        } else {
+            console.log("Файл успешно создан.");
+        }
+        writeStream.end();
+    });
 };
 const csvData = generateDateToSvfFile(COUNT_OF_ROWS);
 
