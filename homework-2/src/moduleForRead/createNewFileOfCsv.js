4e9cf1fae86028877e04a2d65d3bc546f3593169
Docenko Pasha <pashadocenko@gmail.com>
2024-04-04 17:50:13 +0300

    createNewFileOfCsv.js was refactored.

--- homework-2/src/moduleForRead/createNewFileOfCsv.js
+++ homework-2/src/moduleForRead/createNewFileOfCsv.js
@@ -1,13 +1,20 @@
 const createNewFileOfCsv = (data) => {
     let newCsv = "UUID,Price,Quantity,Date\n";
 
-    for (let i = 0; i < data.length; i++) {
-        const uuid = data[i].UUID;
-        const price = data[i].Price;
-        const quantity = data[i].Quantity;
-        const date = data[i].Date;
+    // old code
+    // for (let i = 0; i < data.length; i++) {
+    //     const uuid = data[i].UUID;
+    //     const price = data[i].Price;
+    //     const quantity = data[i].Quantity;
+    //     const date = data[i].Date;
+    //
+    //     newCsv += `${uuid},${price},${quantity},${date}\n`;
+    // }
 
-        newCsv += `${uuid},${price},${quantity},${date}\n`;
+    // new code
+    for (const item of data) {
+        const { UUID, Price, Quantity, Date } = item;
+        newCsv += `${UUID},${Price},${Quantity},${Date}\n`;
     }
 
     return newCsv;
