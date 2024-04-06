24a5a2d0c2bc7718a9224d414761a589bfe09110
Docenko Pasha <pashadocenko@gmail.com>
2024-04-06 12:41:17 +0300

    Routes were added.

--- homework-3/index.js
+++ homework-3/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const routes = require("./routes/index");
 
 const app = express();
 
 
 app.use(express.json());
+app.use("/api" , routes);
 
 const start = async ()=>{
     try{
---
+++ homework-3/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const Router = require("express");
+
+const router = Router();
+
+router.get("/registration" , (req, res) => {
+    return res.status(200).json({message: "Hello world"})
+})
+
+module.exports = router;
