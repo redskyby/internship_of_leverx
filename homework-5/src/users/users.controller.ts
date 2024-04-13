03cbda749584890b71e58354639f056d59a6e944
Docenko Pasha <pashadocenko@gmail.com>
2024-04-13 23:12:31 +0300

    jwt-auth.guard.ts works.

--- homework-5/src/users/users.controller.ts
+++ homework-5/src/users/users.controller.ts
@@ -1,12 +1,7 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -32,10 +27,4 @@ export class UsersController {
     return this.usersService.login(userDto);
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('test')
-  log(@Body() userDto: LoginUserDto) {
-    return this.usersService.login(userDto);
-    return userDto;
-  }
 }
