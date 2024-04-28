2fb4bd5def6b94bc0d11fc8d5fbb06e305ed8a1c
Docenko Pasha <pashadocenko@gmail.com>
2024-04-28 14:28:47 +0300

    purchases.module was added.

--- final/src/app.module.ts
+++ final/src/app.module.ts
@@ -14,6 +14,7 @@ import { Review } from './reviews/entities/review.entity';
 import { StripeModule } from './stripe/stripe.module';
 import { MailModule } from './mail/mail.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
   controllers: [],
@@ -51,6 +52,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ReviewsModule,
     StripeModule,
     MailModule,
+    PurchasesModule,
   ],
 })
 export class AppModule {}
