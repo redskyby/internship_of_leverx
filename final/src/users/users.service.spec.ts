740365eb044ed42e0f59745535b6daec8f6d583c
Docenko Pasha <pashadocenko@gmail.com>
2024-05-03 22:12:05 +0300

    Test of create user was added.

--- final/src/users/users.service.spec.ts
+++ final/src/users/users.service.spec.ts
@@ -1,10 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import {User} from "./entities/user.entity";
-import {getModelToken} from "@nestjs/sequelize";
-import {CreateUserDto} from "./dto/create-user.dto";
-import {RolesService} from "../roles/roles.service";
-import {JwtModule} from "@nestjs/jwt";
+import { User } from './entities/user.entity';
+import { getModelToken } from '@nestjs/sequelize';
+import { CreateUserDto } from './dto/create-user.dto';
+import { RolesService } from '../roles/roles.service';
+import { JwtModule } from '@nestjs/jwt';
+import { AllInformationUserDto } from './dto/all-information-user.dto';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersService', () => {
   let service: UsersService;
@@ -12,7 +14,8 @@ describe('UsersService', () => {
   // Mocked User model with required Sequelize instance methods
   const mockUserModel = {
     create: jest.fn(),
-    findById: jest.fn(),
+    findOne: jest.fn(),
+    destroy: jest.fn(),
   };
 
   // User instance with expected instance methods
@@ -60,11 +63,11 @@ describe('UsersService', () => {
     mockUserModel.create.mockResolvedValue(userWithMethods);
   });
 
-  it('должен быть определен', () => {
+  it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('должен создать пользователя', async () => {
+  it('should be create user with role', async () => {
     const userDto: CreateUserDto = {
       name: 'Павел',
       lastName: 'Доценко',
@@ -91,5 +94,68 @@ describe('UsersService', () => {
     // Ensure the created user has the correct roles
     expect(result.roles).toEqual([role]);
   });
-});
 
+  it('should throw an error when creating a user with an existing email', async () => {
+    const userDto: CreateUserDto = {
+      name: 'Павел',
+      lastName: 'Доценко',
+      password: '12345',
+      email: 'example@example.com',
+      birthdate: new Date('2020-10-12'),
+      avatar: 'funny.jpg',
+    };
+
+    mockUserModel.create.mockRejectedValue(
+      new Error('User with this email already exists'),
+    );
+
+    await expect(service.createUser(userDto)).rejects.toThrow(
+      'User with this email already exists',
+    );
+  });
+
+  it('should show all information', async () => {
+    const email = 'example@example.com';
+    const userDto: AllInformationUserDto = {
+      id: 1,
+      name: 'Павел',
+      lastName: 'Доценко',
+      email: 'example@example.com',
+      birthdate: new Date('2020-10-12').toDateString(),
+      avatar: 'funny.jpg',
+      roles: [],
+      iat: 1,
+      exp: 1,
+    };
+
+    mockUserModel.findOne.mockResolvedValue(userDto.email);
+
+    const result = await service.showUser(userDto);
+
+    expect(mockUserModel.findOne).toHaveBeenCalledWith({ where: { email } });
+
+    expect(result).toEqual(userDto);
+  });
+
+  it('should show NotFoundException', async () => {
+    const userDto: AllInformationUserDto = {
+      id: 1,
+      name: 'Павел',
+      lastName: 'Доценко',
+      email: 'none@none.com',
+      birthdate: new Date('2020-10-12').toDateString(),
+      avatar: 'funny.jpg',
+      roles: [],
+      iat: 1,
+      exp: 1,
+    };
+
+    mockUserModel.findOne.mockResolvedValue(null);
+
+    await expect(service.showUser(userDto)).rejects.toThrow(NotFoundException);
+
+    await expect(service.showUser(userDto)).rejects.toThrow(
+      'Пользователь не найден',
+    );
+  });
+});
