2f42af7986b80210256696a4ca0e5256f1493481
Docenko Pasha <pashadocenko@gmail.com>
2024-05-04 22:53:00 +0300

    users.controller.spec.ts was inited

--- final/src/users/users.controller.spec.ts
+++ final/src/users/users.controller.spec.ts
@@ -1,20 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import {JwtModule} from "@nestjs/jwt";
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let userService : UsersService
+
+
+  const mockUserService = {
+    showUser : jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide : UsersService,
+          useValue : mockUserService
+        }
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    userService = module.get<UsersService>(UsersService)
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+
+
 });
