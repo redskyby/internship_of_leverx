f8099d8012026bb3f46fd02d2b6b55cbb8fb885f
Docenko Pasha <pashadocenko@gmail.com>
2024-05-05 00:46:00 +0300

    vinyls.controller.spec.ts was added new method (create +)

--- final/src/vinyls/vinyls.controller.spec.ts
+++ final/src/vinyls/vinyls.controller.spec.ts
@@ -2,12 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { VinylsController } from './vinyls.controller';
 import { VinylsService } from './vinyls.service';
 import { JwtModule } from '@nestjs/jwt';
+import {CreateVinylDto} from "./dto/create-vinyl.dto";
 
 describe('VinylsController', () => {
   let controller: VinylsController;
   let vinylService: VinylsService;
 
-  const mockVinylService = {};
+  const mockVinylService = {
+    create : jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,7 +33,31 @@ describe('VinylsController', () => {
     vinylService = module.get<VinylsService>(VinylsService);
   });
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a vinyl', async () => {
+      const vinyl : CreateVinylDto = {
+        name : "newVinyl",
+        price : 10,
+        author : "Pasha",
+        description : "song for Gods"
+      }
+
+      const mockVinyl : CreateVinylDto = {
+        name : "newVinyl1",
+        price : 12,
+        author : "Pasha2",
+        description : "song for Gods2"
+      }
+
+      mockVinylService.create.mockResolvedValue(mockVinyl)
+
+      const result = await controller.create(vinyl);
+
+      await expect(vinylService.create).toHaveBeenCalledWith(vinyl)
+    await  expect(result).toEqual(mockVinyl);
+
   });
 });
