8c041e98150c8e3e061975c2cadc826b534a0834
Docenko Pasha <pashadocenko@gmail.com>
2024-05-05 00:30:48 +0300

    vinyls.controller.spec.ts was added new method (toBeDefined)

--- final/src/vinyls/vinyls.controller.spec.ts
+++ final/src/vinyls/vinyls.controller.spec.ts
@@ -1,17 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VinylsController } from './vinyls.controller';
 import { VinylsService } from './vinyls.service';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('VinylsController', () => {
   let controller: VinylsController;
+  let vinylService: VinylsService;
+
+  const mockVinylService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [VinylsController],
-      providers: [VinylsService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide: VinylsService,
+          useValue: mockVinylService,
+        },
+      ],
     }).compile();
 
     controller = module.get<VinylsController>(VinylsController);
+    vinylService = module.get<VinylsService>(VinylsService);
   });
 
   it('should be defined', () => {
