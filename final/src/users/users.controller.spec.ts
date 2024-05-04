6567f63c97738bcb63ebfc3c032ec67b9cd74579
Docenko Pasha <pashadocenko@gmail.com>
2024-05-04 23:04:33 +0300

    users.controller.spec.ts was added a new method (showUser).

--- final/src/users/users.controller.spec.ts
+++ final/src/users/users.controller.spec.ts
@@ -1,16 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import {JwtModule} from "@nestjs/jwt";
-
+import { JwtModule } from '@nestjs/jwt';
+import {AllInformationUserDto} from "./dto/all-information-user.dto";
+import { Request} from 'express';
 describe('UsersController', () => {
   let controller: UsersController;
-  let userService : UsersService
-
+  let userService: UsersService;
 
   const mockUserService = {
-    showUser : jest.fn()
-  }
+    showUser: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,20 +23,51 @@ describe('UsersController', () => {
       ],
       providers: [
         {
-          provide : UsersService,
-          useValue : mockUserService
-        }
+          provide: UsersService,
+          useValue: mockUserService,
+        },
       ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
-    userService = module.get<UsersService>(UsersService)
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 
+  it('should return all information of user', async () => {
+    const user: AllInformationUserDto = {
+      id: 1,
+      name: 'Павел',
+      lastName: 'Доценко',
+      email: 'none@none.com',
+      birthdate: new Date('2020-10-12').toDateString(),
+      avatar: 'funny.jpg',
+      roles: [],
+      iat: 1,
+      exp: 1,
+    };
 
+    const mockUser: AllInformationUserDto = {
+      id: 2,
+      name: 'Павел2',
+      lastName: 'Доценко2',
+      email: 'none@none2.com',
+      birthdate: new Date('2020-10-12').toDateString(),
+      avatar: 'funny.jpg',
+      roles: [],
+      iat: 1,
+      exp: 1,
+    };
 
+    mockUserService.showUser.mockResolvedValue(mockUser);
+
+    const result = await controller.profile({ user } as Request);
+
+    await expect(userService.showUser).toHaveBeenCalledWith(user);
+await  expect(result).toEqual(mockUser)
+
+  });
 });
