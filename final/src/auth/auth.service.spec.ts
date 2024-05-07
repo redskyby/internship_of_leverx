155cc7f030f43faaaf3284e949d531b27476d48b
Docenko Pasha <pashadocenko@gmail.com>
2024-05-07 21:23:53 +0300

    auth.service.spec.ts was inited.

--- final/src/auth/auth.service.spec.ts
+++ final/src/auth/auth.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockAuthService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
