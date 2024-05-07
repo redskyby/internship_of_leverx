737f3a1754fbd80aadd84e905b2a0e757024636e
Docenko Pasha <pashadocenko@gmail.com>
2024-05-07 22:57:18 +0300

    purchases.controller.spec.ts was inited.

--- final/src/purchases/purchases.controller.spec.ts
+++ final/src/purchases/purchases.controller.spec.ts
@@ -1,17 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PurchasesController } from './purchases.controller';
 import { PurchasesService } from './purchases.service';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('PurchasesController', () => {
   let controller: PurchasesController;
+  let purchasesService: PurchasesService;
+
+  const mockPurchasesService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PurchasesController],
-      providers: [PurchasesService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide: PurchasesService,
+          useValue: mockPurchasesService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PurchasesController>(PurchasesController);
+    purchasesService = module.get<PurchasesService>(PurchasesService);
   });
 
   it('should be defined', () => {
