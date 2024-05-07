1f86594eb367cb01d40ba12b222e28ac23e84ebd
Docenko Pasha <pashadocenko@gmail.com>
2024-05-07 23:19:19 +0300

    vinyls.service.spec.ts was inited.

--- final/src/vinyls/vinyls.service.spec.ts
+++ final/src/vinyls/vinyls.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VinylsService } from './vinyls.service';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('VinylsService', () => {
   let service: VinylsService;
 
+  const mockVinylService = {};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VinylsService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide: VinylsService,
+          useValue: mockVinylService,
+        },
+      ],
     }).compile();
 
     service = module.get<VinylsService>(VinylsService);
