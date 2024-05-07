a1891beed0095521ccbb5a96405c394a04c108fe
Docenko Pasha <pashadocenko@gmail.com>
2024-05-07 23:05:13 +0300

    reviews.controller.spec.ts was inited.

--- final/src/reviews/reviews.controller.spec.ts
+++ final/src/reviews/reviews.controller.spec.ts
@@ -1,17 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('ReviewsController', () => {
   let controller: ReviewsController;
+  let reviewsService: ReviewsService;
+
+  const mockReviewsService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReviewsController],
-      providers: [ReviewsService],
+      imports: [
+        JwtModule.register({
+          secret: 'test-secret',
+          signOptions: { expiresIn: '1h' },
+        }),
+      ],
+      providers: [
+        {
+          provide: ReviewsService,
+          useValue: mockReviewsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReviewsController>(ReviewsController);
+    reviewsService = module.get<ReviewsService>(ReviewsService);
   });
 
   it('should be defined', () => {
