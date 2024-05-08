c8436376e2f6b372c754a6f2054bf4ccd34b7df5
Docenko Pasha <pashadocenko@gmail.com>
2024-05-09 00:24:16 +0300

    vinyls.service.spec.ts was inited (create + )

--- final/src/vinyls/vinyls.service.spec.ts
+++ final/src/vinyls/vinyls.service.spec.ts
@@ -1,31 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VinylsService } from './vinyls.service';
 import { JwtModule } from '@nestjs/jwt';
+import { getModelToken } from '@nestjs/sequelize';
+import { Vinyl } from './entities/vinyl.entity';
+import { Review } from '../reviews/entities/review.entity';
+import { CreateVinylDto } from './dto/create-vinyl.dto';
 
 describe('VinylsService', () => {
   let service: VinylsService;
 
-  const mockVinylService = {};
+  const mockVinylRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    findAll: jest.fn(),
+    update: jest.fn(),
+    destroy: jest.fn(),
+  };
+
+  const mockReviewRepository = {
+    findAll: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        JwtModule.register({
-          secret: 'test-secret',
-          signOptions: { expiresIn: '1h' },
-        }),
-      ],
       providers: [
+        VinylsService,
         {
-          provide: VinylsService,
-          useValue: mockVinylService,
+          provide: getModelToken(Vinyl),
+          useValue: mockVinylRepository,
+        },
+        {
+          provide: getModelToken(Review),
+          useValue: mockReviewRepository,
         },
       ],
     }).compile();
 
     service = module.get<VinylsService>(VinylsService);
   });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create Vinyl', async () => {
+    const vinyl: CreateVinylDto = {
+      name: 'newVinyl',
+      price: 10,
+      author: 'Pasha',
+      description: 'song for Gods',
+    };
+
+    mockVinylRepository.create.mockResolvedValue(vinyl);
+
+    const result = await service.create(vinyl);
+
+    expect(mockVinylRepository.create).toHaveBeenCalledWith(vinyl);
+    expect(result).toEqual(vinyl);
   });
 });
