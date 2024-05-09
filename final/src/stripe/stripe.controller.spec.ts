74eaec210f362fe502b8b35a773a64d9b9053e7f
Docenko Pasha <pashadocenko@gmail.com>
2024-05-10 00:55:46 +0300

    stripe.controller.spec.ts was inited (createPayments +)

--- final/src/stripe/stripe.controller.spec.ts
+++ final/src/stripe/stripe.controller.spec.ts
@@ -2,12 +2,23 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StripeController } from './stripe.controller';
 import { StripeService } from './stripe.service';
 import { JwtModule } from '@nestjs/jwt';
+import { PurchasesService } from '../purchases/purchases.service';
+import { CreateStripeDto } from './dto/create-stripe.dto';
+import { AllInformationUserDto } from '../users/dto/all-information-user.dto';
+import { Request } from 'express';
 
 describe('StripeController', () => {
   let controller: StripeController;
   let stripeService: StripeService;
 
-  const mockStripeService = {};
+  const mockStripeService = {
+    createPayments: jest.fn(),
+  };
+
+  const mockPurchasesService = {
+    findUserByEmail: jest.fn(),
+    findPurchaseById: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,6 +34,10 @@ describe('StripeController', () => {
           provide: StripeService,
           useValue: mockStripeService,
         },
+        {
+          provide: PurchasesService,
+          useValue: mockPurchasesService,
+        },
       ],
     }).compile();
 
@@ -32,5 +47,37 @@ describe('StripeController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a payment', async () => {
+    const createStripeDto: CreateStripeDto = {
+      email: 'pashadocenko@gmail.com',
+      vinylId: 1,
+    };
+
+    const user: AllInformationUserDto = {
+      name: 'Pavel',
+      id: 1,
+      lastName: 'Dotsenko',
+      email: 'pashadocenko@gmail.com',
+      birthdate: '2020-10-12',
+      avatar: 'funny.jpg',
+      roles: [],
+      iat: 1744521,
+      exp: 45484,
+    };
+
+    const mockAnswer = { url: 'http://example.com/success' };
+
+    mockStripeService.createPayments.mockResolvedValue(mockAnswer);
+
+    const result = await controller.create(createStripeDto, {
+      user,
+    } as Request);
+    await expect(stripeService.createPayments).toHaveBeenCalledWith(
+      createStripeDto,
+      user,
+    );
+    await expect(result).toEqual(mockAnswer);
   });
 });
