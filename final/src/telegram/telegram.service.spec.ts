1f4f1574bd7fa805b865697a481888bef42d7bc3
Docenko Pasha <pashadocenko@gmail.com>
2024-05-10 00:18:17 +0300

    telegram.service.spec.ts was inited (sendMessage +)

--- final/src/telegram/telegram.service.spec.ts
+++ final/src/telegram/telegram.service.spec.ts
@@ -5,7 +5,10 @@ import { JwtModule } from '@nestjs/jwt';
 describe('TelegramService', () => {
   let service: TelegramService;
 
-  const mockTelegramService = {};
+  const mockTelegramService = {
+    findById: jest.fn(),
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,5 +31,29 @@ describe('TelegramService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a message', async () => {
+    const id: number = 1;
+    const vinyl = {
+      id: 1,
+      name: 'test',
+      price: 999,
+      author: 'The Beatles',
+      description: 'test',
+      createdAt: '2024-04-28T19:28:12.000Z',
+      updatedAt: '2024-04-28T19:36:26.000Z',
+      avgRating: '1.0000',
+    };
+
+    const mockAnswer = { message: 'Сообщение успешно отправлено' };
+
+    mockTelegramService.findById.mockResolvedValue(vinyl);
+    mockTelegramService.create.mockResolvedValue(mockAnswer);
+
+    const result = await service.create(id);
+
+    await expect(mockTelegramService.create).toHaveBeenCalledWith(id);
+    await expect(result).toEqual(mockAnswer);
   });
 });
