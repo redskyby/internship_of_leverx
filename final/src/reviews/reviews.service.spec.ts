6adedb6710b2b8cbfea05ddc46da470d84291bc4
Docenko Pasha <pashadocenko@gmail.com>
2024-05-11 17:23:37 +0300

    reviews.service.spec.ts was inited (create +)

--- final/src/reviews/reviews.service.spec.ts
+++ final/src/reviews/reviews.service.spec.ts
@@ -1,11 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewsService } from './reviews.service';
 import { JwtModule } from '@nestjs/jwt';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { VinylsService } from '../vinyls/vinyls.service';
+import { UsersService } from '../users/users.service';
 
 describe('ReviewsService', () => {
   let service: ReviewsService;
 
-  const mockReviewsService = {};
+  const mockReviewRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    findAll: jest.fn(),
+    destroy: jest.fn(),
+  };
+
+  const mockVinylRepository = {
+    findOne: jest.fn(),
+  };
+
+  const mockUserRepository = {
+    findOne: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
@@ -15,9 +32,18 @@ describe('ReviewsService', () => {
         }),
       ],
       providers: [
+        ReviewsService,
         {
           provide: ReviewsService,
-          useValue: mockReviewsService,
+          useValue: mockReviewRepository,
+        },
+        {
+          provide: VinylsService,
+          useValue: mockVinylRepository,
+        },
+        {
+          provide: UsersService,
+          useValue: mockUserRepository,
         },
       ],
     }).compile();
@@ -27,5 +53,26 @@ describe('ReviewsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a review', async () => {
+    const createReviewDto: CreateReviewDto = {
+      review: 10,
+      vinylId: 1,
+      userId: 1,
+    };
+
+    const mockReviewDto = {
+      review: 10,
+      vinylId: 1,
+      userId: 1,
+    };
+
+    mockReviewRepository.create.mockResolvedValue(mockReviewDto)
+
+    const result = await service.create(createReviewDto)
+
+    await expect(mockReviewRepository.create).toHaveBeenCalledWith(createReviewDto)
+    await expect(result).toEqual(mockReviewDto)
   });
 });
