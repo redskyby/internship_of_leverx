1c761f80ba63e028ac26807d8f239c3fb8cab5ba
Docenko Pasha <pashadocenko@gmail.com>
2024-05-11 17:36:53 +0300

    reviews.service.spec.ts was inited (findAll -+)

--- final/src/reviews/reviews.service.spec.ts
+++ final/src/reviews/reviews.service.spec.ts
@@ -4,7 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { VinylsService } from '../vinyls/vinyls.service';
 import { UsersService } from '../users/users.service';
-import {NotFoundException} from "@nestjs/common";
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('ReviewsService', () => {
   let service: ReviewsService;
@@ -69,12 +69,14 @@ describe('ReviewsService', () => {
       userId: 1,
     };
 
-    mockReviewRepository.create.mockResolvedValue(mockReviewDto)
+    mockReviewRepository.create.mockResolvedValue(mockReviewDto);
 
-    const result = await service.create(createReviewDto)
+    const result = await service.create(createReviewDto);
 
-    await expect(mockReviewRepository.create).toHaveBeenCalledWith(createReviewDto)
-    await expect(result).toEqual(mockReviewDto)
+    await expect(mockReviewRepository.create).toHaveBeenCalledWith(
+      createReviewDto,
+    );
+    await expect(result).toEqual(mockReviewDto);
   });
 
   it('should throw NotFoundException if user does not exist', async () => {
@@ -84,9 +86,60 @@ describe('ReviewsService', () => {
       userId: 1,
     };
 
-    mockReviewRepository.create.mockRejectedValue(new NotFoundException('Такого пользователя не существует.'))
+    mockReviewRepository.create.mockRejectedValue(
+      new NotFoundException('Такого пользователя не существует.'),
+    );
 
-    await expect(service.create(createReviewDto)).rejects.toThrow(NotFoundException);
-    await expect(service.create(createReviewDto)).rejects.toThrow('Такого пользователя не существует.');
+    await expect(service.create(createReviewDto)).rejects.toThrow(
+      NotFoundException,
+    );
+    await expect(service.create(createReviewDto)).rejects.toThrow(
+      'Такого пользователя не существует.',
+    );
+  });
+
+  it('should find all reviews', async () => {
+    const offset = 0;
+    const limit = 10;
+    const mockReviews = [
+      {
+        review: 10,
+        vinylId: 1,
+        userId: 1,
+      },
+      {
+        review: 9,
+        vinylId: 2,
+        userId: 1,
+      },
+    ];
+
+    mockReviewRepository.findAll.mockResolvedValueOnce(mockReviews);
+
+    const result = await service.findAll(offset, limit);
+
+    await expect(mockReviewRepository.findAll).toHaveBeenCalledWith(
+      offset,
+      limit,
+    );
+    await expect(result).toEqual(mockReviews);
+  });
+
+  it('should throw BadRequestException if no reviews found', async () => {
+    const offset = 0;
+    const limit = 10;
+
+    mockReviewRepository.findAll.mockRejectedValue(
+      new BadRequestException(
+        'Задайте другие настройки поиска или список пуст.',
+      ),
+    );
+
+    await expect(service.findAll(offset, limit)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(service.findAll(offset, limit)).rejects.toThrow(
+      'Задайте другие настройки поиска или список пуст.',
+    );
   });
 });
