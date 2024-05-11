6c47dcf708df9879e9073bc2fff33207e9ed2caf
Docenko Pasha <pashadocenko@gmail.com>
2024-05-11 16:07:09 +0300

    roles.controller.spec.ts was inited (create +)

--- final/src/roles/roles.controller.spec.ts
+++ final/src/roles/roles.controller.spec.ts
@@ -2,12 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { JwtModule } from '@nestjs/jwt';
+import {CreateRoleDto} from "./dto/create-role.dto";
 
 describe('RolesController', () => {
   let controller: RolesController;
   let rolesService: RolesService;
 
-  const mockRolesService = {};
+  const mockRolesService = {
+    create: jest.fn(),
+    getRoleByValue: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -33,4 +37,19 @@ describe('RolesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a new role', async () => {
+    const roleDto: CreateRoleDto = { value: 'test_role' };
+
+    const createdRole = { id: 1, ...roleDto , users : [] };
+
+    mockRolesService.create.mockResolvedValue(createdRole);
+
+    const result = await controller.create(roleDto);
+
+    await expect(result).toEqual(createdRole);
+    await expect(mockRolesService.create).toHaveBeenCalledWith(roleDto);
+  });
+
+
 });
--- final/src/stripe/stripe.service.spec.ts
+++ final/src/stripe/stripe.service.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StripeService } from './stripe.service';
 import { JwtModule } from '@nestjs/jwt';
+import {PurchasesService} from "../purchases/purchases.service";
+import {MailService} from "../mail/mail.service";
 
 describe('StripeService', () => {
   let service: StripeService;
 
-  const mockStripeService = {};
+  const mockStripeService = {
+    createPayments: jest.fn(),
+    success: jest.fn(),
+    cancel: jest.fn(),
+  };
+  const mockMailService = {
+    sendNewInformation: jest.fn(),
+  };
+
+  const mockPurchasesService = {
+    findUserByEmail: jest.fn(),
+    findPurchaseById: jest.fn(),
+    remove: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,6 +35,14 @@ describe('StripeService', () => {
           provide: StripeService,
           useValue: mockStripeService,
         },
+        {
+          provide: PurchasesService,
+          useValue: mockPurchasesService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
       ],
     }).compile();
 
@@ -29,4 +52,5 @@ describe('StripeService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
